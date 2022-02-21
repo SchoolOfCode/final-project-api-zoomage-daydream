4ed91f2b4fc261bdbbe453bd67dcd7aa57510a70
RobertHJones <roberthuwjones@outlook.com>
2022-02-21 10:42:33 +0000

    table created

--- models/users.js
+++ models/users.js
@@ -4,3 +4,24 @@ export async function getAllUsers() {
   const result = await db.query(`SELECT * FROM users;`);
   return result.rows;
 }
+
+// Get all spaces from spaces table
+export async function getAllSpaces() {
+  const result = await db.query(`SELECT * FROM spaces`);
+  return result.rows;
+}
+
+// Search for a space by ID
+export async function getSpaceByID(id) {
+  const result = await db.query(`SELECT * FROM spaces WHERE id = $1;`, [id]);
+  return result.rows;
+}
+
+// More specific search for a space, to be used on the reserve on home page
+export async function getSpaceBySearch(location, date, time, type) {
+  const result = await db.query(
+    `SELECT * FROM spaces WHERE address ILIKE '%' || $1 || '%' AND  type_of_space = $2 AND days_available `,
+    [location, type]
+  );
+  return result.rows;
+}
---
+++ tables.js
@@ -0,0 +1,51 @@
+export const users = [
+  {
+    full_name: "Kazeem Alatishe",
+    username: "Cazim",
+    email: "kaz-a@gmail.com",
+    date_of_birth: 21 / 02 / 1970,
+  },
+  {
+    full_name: "Ayomide Ojelade",
+    username: "Ayomide",
+    email: "ayomide@gmail.com",
+    date_of_birth: 12 / 05 / 1971,
+  },
+  {
+    full_name: "Robert Jones",
+    username: "BobbyJ",
+    email: "rj@gmail.com",
+    date_of_birth: 01 / 05 / 1936,
+  },
+];
+
+export const spaces = [
+  {
+    address: "12 Nowhere Road, Birmingham, UK",
+    type_of_space: "house",
+    purpose_of_space: "remote working",
+    fraction_of_space: "private room",
+    amenities: "WiFi, shower, fridge",
+    additional_information: "Exceptional view of rolling hills",
+    days_available: [Monday, Tuesday, Wednesday, Thursday],
+    times: { start_time: "08:00", end_time: "17:00" },
+    images: [
+      "https://images.pexels.com/photos/186077/pexels-photo-186077.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+      "https://images.pexels.com/photos/2724748/pexels-photo-2724748.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+    ],
+  },
+  {
+    address: "11 Nowhere Road, Birmingham, UK",
+    type_of_space: "house",
+    purpose_of_space: "remote working",
+    fraction_of_space: "private room",
+    amenities: "WiFi, shower, fridge",
+    additional_information: "Exceptional view of rolling hills",
+    days_available: [Monday, Tuesday, Wednesday, Thursday],
+    times: { start_time: "08:00", end_time: "17:00" },
+    images: [
+      "https://images.pexels.com/photos/186077/pexels-photo-186077.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+      "https://images.pexels.com/photos/2724748/pexels-photo-2724748.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+    ],
+  },
+];
