7890e7332e358323c713f47857b905709b1446c8
RobertHJones <roberthuwjones@outlook.com>
2022-02-23 10:26:36 +0000

    post request test

--- db/connection.js
+++ db/connection.js
@@ -2,8 +2,10 @@ import pg from "pg";
 
 import { connectionString } from "../config.js";
 
+console.log(connectionString);
+
 const pool = new pg.Pool({
-connectionString,
+  connectionString,
   ssl: {
     rejectUnauthorized: false,
   },
--- routes/users.test.js
+++ routes/users.test.js
@@ -1,46 +1,59 @@
-import request from "supertest"
-import app from "../app.js"
+import request from "supertest";
+import app from "../app.js";
 
-const user =   {
-  id:expect.any(Number),
-    full_name: expect.any(String),
-    username: expect.any(String),
-    email: expect.any(String),
-    date_of_birth: expect.any(String),
-  }
-  
+const user = {
+  full_name: expect.any(String),
+  email: expect.any(String),
+  date_of_birth: expect.any(String),
+  id: expect.any(Number),
+  username: expect.any(String),
+};
+
 // tests for the users table
-describe("Get user for the users route works as expected", ()=>{
+describe("Get user for the users route works as expected", () => {
+  // testing the status code to see it returns the right status code
+  test("should return a status of 200", async () => {
+    //  arrange
+    const res = await request(app).get("/users");
+    // assert
+    expect(res.statusCode).toBe(200);
+  });
 
- // testing the status code to see it returns the right status code
- test('should return a status of 200', async () => { 
-  //  arrange
-  const res = await  request(app).get("/users")
-  // assert
-  expect(res.statusCode).toBe(200)
- })
-
- // testing the response body to see it return the actual content
- test('should return the actual user', async () => { 
-  const res = await  request(app).get("/users")
-  // arrange
-  const actual = res.body
-   const expected = {
+  // testing the response body to see it return the actual content
+  test("should return the actual user", async () => {
+    const res = await request(app).get("/users");
+    // arrange
+    const actual = res.body;
+    const expected = {
       success: true,
-      payload: "expect.any(Array)"
+      payload: expect.any(Array),
     };
     // assert
-  expect(actual).toEqual(expected)
- })
+    console.log(actual, "is the actual");
+    expect(actual).toEqual(expected);
+  });
 
-//  test"Should return the actual payload expected"
- test('should return the actual payload', async () => { 
-  const res = await  request(app).get("/users")
-  // arrange
-  const actual = res.body
-  actual.payload.forEach((item) => {
-    // assert
-  expect(item).toEqual(user);
+  //  test"Should return the actual payload expected"
+  test("should return the actual payload", async () => {
+    const res = await request(app).get("/users");
+    // arrange
+    const actual = res.body;
+    actual.payload.forEach((item) => {
+      // assert
+      expect(item).toEqual(user);
     });
- })
-}) 
+  });
+});
+
+// testing the post request
+// test("should post to the users table", async () => {
+//   const data = {
+//     full_name: "Robert Jones",
+//     email: "robertjones@mail.com",
+//     date_of_birth: "1955-05-12",
+//     username: "BobbyJ"
+//   }
+//   await request(app).post("/users").send(data).expect(200).then(async (response) => {
+
+//   })
+// };
