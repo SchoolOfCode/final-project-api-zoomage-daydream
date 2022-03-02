859300f8730c1bc44d74be6a5a6ddd70ce183095
RobertHJones <roberthuwjones@outlook.com>
2022-03-02 16:51:20 +0000

    date

--- db/scripts/users/createUserTable.js
+++ db/scripts/users/createUserTable.js
@@ -1,7 +1,7 @@
 import db from "../../connection.js";
 console.log(db);
 const response = await db.query(
-  `CREATE TABLE IF NOT EXISTS users (id SERIAL PRIMARY KEY, full_name VARCHAR, username VARCHAR, email VARCHAR, date_of_birth VARCHAR);`
+  `CREATE TABLE IF NOT EXISTS users (id SERIAL PRIMARY KEY, full_name VARCHAR, username VARCHAR, email VARCHAR, date_of_birth DATE);`
 );
 
 console.log(response);
